c436c40718fffd554e965f9ab54f628320edef45
hormalno <ivanov.yasen@gmail.com>
2025-07-10 00:53:49 +0300

    testing ci/cd integration

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import MoodNavbar from "../components/MoodNavbar";
 const Home = () => {
     return (
         <div>
+            <h1>Welcome to the Mood Tracker</h1>
+            <p>Track your mood and see your progress over time.</p>
             <MoodNavbar />
             <MoodCalendar />
         </div>

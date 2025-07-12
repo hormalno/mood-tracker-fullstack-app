2a7700d27538a0f106b65a5e6f1a1173e7aec33e
hormalno <ivanov.yasen@gmail.com>
2025-07-12 20:57:52 +0300

    updated path for user in frontend

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,7 +1,7 @@
 import api from "../services/Axios";
 
 export async function login(username: string, password: string) {
-  const response = await api.post('/api/user/login', { username, password });
+  const response = await api.post('/user/login', { username, password });
   const { access_token } = response.data;
   if (access_token) {
     localStorage.setItem('token', access_token);
@@ -10,6 +10,6 @@ export async function login(username: string, password: string) {
 }
 
 export async function register(username: string, password: string) {
-  const response = await api.post('/api/user/register', { username, password });
+  const response = await api.post('/user/register', { username, password });
   return response.data;
 }

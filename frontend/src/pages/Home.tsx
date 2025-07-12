535c7c588faf61c6c4676b71d8d88ae705d5680b
hormalno <ivanov.yasen@gmail.com>
2025-07-12 22:51:55 +0300

    updated home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -5,6 +5,8 @@ const Home = () => {
     return (
         <div>
             <MoodNavbar />
+            <h1>Welcome to the Mood Tracker</h1>
+            <p>Track your mood and see your progress over time.</p>
             <MoodCalendar />
         </div>
     );

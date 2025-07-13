797f253f8b599ebe7ff8a378c859c669e896a05c
hormalno <ivanov.yasen@gmail.com>
2025-07-13 11:01:26 +0300

    testing ci/cd with additional health check of the api

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.api import mood, user
+from app.api import mood, user, ping
 from app.core.db import engine, Base
 
 Base.metadata.create_all(bind=engine)
@@ -22,3 +22,4 @@ app.add_middleware(
 
 app.include_router(mood.router, prefix=f"{settings.API_V1_STR}/mood", tags=["Mood"])
 app.include_router(user.router, prefix=f"{settings.API_V1_STR}/user", tags=["User"])
+app.include_router(ping.router, prefix=f"{settings.API_V1_STR}/health", tags=["Ping"])
